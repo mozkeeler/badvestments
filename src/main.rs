c2ac683a789797190c82b3fc7fb7a3dae08819f0
David Keeler <mozkeeler@gmail.com>
2017-08-08 13:31:15 -0700

    add more rules, implement posting to twitter

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
+extern crate oauth_client as oath;
 extern crate rand;
+extern crate twitter_api as twitter;
 
 pub mod badvestments;
 pub mod ast;
 
+use oath::Token;
 use std::fs::File;
-use std::io::BufReader;
-use std::io::BufRead;
+use std::io::{self, BufRead, BufReader, Write};
 use std::collections::HashMap;
-
 use rand::Rng;
 
 struct SubstitutionMap {
@@ -53,6 +54,63 @@ impl SubstitutionMap {
     }
 }
 
+fn prompt(msg: &str) -> String {
+    print!("{}: ", msg);
+    io::stdout().flush().unwrap();
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    return line.trim().to_string();
+}
+
+struct AccessToken {
+    pub consumer_key: String,
+    pub consumer_secret: String,
+    pub access_key: String,
+    pub access_secret: String,
+}
+
+impl AccessToken {
+    pub fn read() -> Option<AccessToken> {
+        let mut token = AccessToken { consumer_key: String::new(), consumer_secret: String::new(),
+                                      access_key: String::new(), access_secret: String::new() };
+        let file = match File::open("badvestments.keys") {
+            Ok(f) => f,
+            Err(_) => return None,
+        };
+        let reader = BufReader::new(&file);
+        let mut lines = reader.lines();
+        token.consumer_key = match lines.next() {
+            Some(result) => match result {
+                Ok(val) => val,
+                Err(_) => return None,
+            },
+            None => return None,
+        };
+        token.consumer_secret = match lines.next() {
+            Some(result) => match result {
+                Ok(val) => val,
+                Err(_) => return None,
+            },
+            None => return None,
+        };
+        token.access_key = match lines.next() {
+            Some(result) => match result {
+                Ok(val) => val,
+                Err(_) => return None,
+            },
+            None => return None,
+        };
+        token.access_secret = match lines.next() {
+            Some(result) => match result {
+                Ok(val) => val,
+                Err(_) => return None,
+            },
+            None => return None,
+        };
+        return Some(token);
+    }
+}
+
 fn main() {
     let file = File::open("badvestments.rules").unwrap();
     let reader = BufReader::new(&file);
@@ -79,5 +137,29 @@ fn main() {
                                           .map(|s| { s.replace('"', "") })
                                           .collect();
 
-    println!("{}", results.join(" "));
+    let tweet = results.join(" ");
+    println!("{}", tweet);
+    let token = match AccessToken::read() {
+        Some(t) => t,
+        None => {
+            println!("log in to twitter and visit https://apps.twitter.com/app");
+            println!("go to the 'Keys and Access Tokens' tab and click");
+            println!("'(Re)generate My Access Token and Token Secret'");
+            println!("Copy your Consumer Key, Consumer Secret, Access Token, and");
+            println!("Access Token Secret and save them in badvestments.keys");
+            panic!("couldn't read access tokens");
+        },
+    };
+    let consumer = Token::new(token.consumer_key, token.consumer_secret);
+    let access = Token::new(token.access_key, token.access_secret);
+
+    let doit = prompt("tweet? Y/N");
+    match doit.as_ref() {
+        "Y" => {
+            twitter::update_status(&consumer, &access, &tweet).unwrap();
+        },
+        _ => {
+            println!("ok - didn't tweet that.");
+        }
+    }
 }
